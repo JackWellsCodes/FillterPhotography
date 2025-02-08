7e762652eead25ba91b789c6652e5924cc81212c
Jack Wells <jackwellsapp@gmail.com>
2025-02-08 13:24:47 +0000

    all photo filters added

--- image_editor_folder/image_editor.py
+++ image_editor_folder/image_editor.py
@@ -8,6 +8,10 @@ for filename in os.listdir(path):
     img = Image.open(f'{path}/{filename}')
     edit = img.filter(ImageFilter.SHARPEN).convert('L')
 
+    factor = 1.5
+    enhancer = ImageEnhance.Contrast(edit)
+    edit = enhancer.enhance(factor)
+
     clean_name = os.path.splitext(filename)[0]
     edit.save(f'{pathOut}/{clean_name}.png', 'png')
 
